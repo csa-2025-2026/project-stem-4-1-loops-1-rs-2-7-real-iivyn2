b32f4b5cd5496279bca8be4ea74470d6ae9f6a82
iivyn2 <idnguyen101@student.hbuhsd.edu>
2025-10-17 21:46:17 +0000

    kayla

--- Main.java
+++ Main.java
@@ -2,8 +2,33 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void main(String[] args)
+   public static void main(String[] args)
   {
     // write solutions to problems here
+    Scanner scan = new Scanner(System.in);
+   /* System.out.println("enter numbers to add. Enter -1 to stop");
+
+  int N = 0;
+  int sum = 0; // my running total  
+
+    while (N != -1) // as long as we do not enter -1 
+    {
+      N = scan.nextInt();
+      sum += N; 
+    }
+    sum++;
+    System.out.println("The sum is: " + sum);
+
+    scan.close();
+  
+  */
+
+ //Problem 2 
+  System.out.println("How many numbers do you want to input?");
+
+  while ()
+
+
+
   }
 }
